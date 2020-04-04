81bb776c2b4cff0825ee15fdf7e759505b2ea7f1
ellemonke <ellemonke@gmail.com>
2020-04-03 19:05:08 -0700

    heatmap2

--- static/js/heatmap.js
+++ static/js/heatmap.js
@@ -1,36 +1,37 @@
-var myMap = L.map("map", {
-  center: [20,-20],
-  // center: [37.7749, -122.4194],
-  zoom: 3
-});
 
-L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
-  maxZoom: 18,
-  id: "mapbox.streets",
-  accessToken: API_KEY
-}).addTo(myMap);
 
 var data = "static/resources/confirmed.json";
 
 d3.json(data, function(response) {
-
-  console.log(response);
 
   var heatArray = [];
 
   for (var i = 0; i < response.length; i++) {
     var lat = response[i].Lat;
     var long = response[i].Long;
+    var infections = response[i]['4/2/20']/20;
 
-    if (location) {
-      heatArray.push([lat, long]);
+    if (infections) {
+      heatArray.push([lat, long, infections]);
     }
   }
 
+  var myMap = L.map("map", {
+    center: [20,-20],
+    // center: [37.7749, -122.4194],
+    zoom: 3
+  });
+  
+  L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: `Confirmed Cases: ${response.length} Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>`,
+    maxZoom: 18,
+    id: "mapbox.streets",
+    accessToken: API_KEY
+  }).addTo(myMap);
+
   var heat = L.heatLayer(heatArray, {
-    radius: 50,
-    blur: 5
+    radius: 20,
+    blur: 15
   }).addTo(myMap);
 
 });
