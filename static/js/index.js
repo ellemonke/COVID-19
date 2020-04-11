8e642e5a53e81d3127069f1d86bbf045f95aeee1
Ellen Hsu <ixnayontheamspay@gmail.com>
2020-04-11 16:03:20 -0700

    scatter plots

--- static/js/index.js
+++ static/js/index.js
@@ -2,8 +2,8 @@
 var country = d3.select("#country");
 
 // Output
-var tested = d3.select("#tested");
 var confirmed = d3.select("#confirmed");
+var recovered = d3.select("#recovered");
 var fatalities = d3.select("#fatalities");
 var lineChart = d3.select(".lineChart");
 var barChart = d3.select(".barChart");
@@ -17,23 +17,23 @@ function onLoad() {
 
 // onChange
 function changeOutput() {
-    // Reset data from confirmed.js
+    // Reset data from all.json
     var results = data;
  
     // Filter data
     var selection = country.property("value");
-    results = results.filter(result => result.properties["Country/Region"] === selection);
-    result = results[0].properties;
+    results = results.filter(result => result["Country_Region"] === selection);
+    result = results[0];
     
     // Assign results
-    var stat_t = result["4/7/20"]; 
-    var stat_c = result["4/7/20"]; 
-    var stat_f = result["4/7/20"]; 
+    var stat_c = result["Confirmed"].toLocaleString(); 
+    var stat_r = result["Recovered"].toLocaleString(); 
+    var stat_f = result["Fatalities"].toLocaleString(); 
     var country_name = selection.replace(' ', '').toLowerCase();
     
     // Update output
-    tested.text(stat_t);
     confirmed.text(stat_c);
+    recovered.text(stat_r);
     fatalities.text(stat_f);
     lineChart.attr("src", `static/images/confirmed_${country_name}.png`);
     barChart.attr("src", `static/images/cvf_${country_name}.png`);    
