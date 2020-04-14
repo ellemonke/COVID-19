88595626c1b81c4b59789ec313f7524000440e6c
Ellen Hsu <ixnayontheamspay@gmail.com>
2020-04-14 13:01:43 -0700

    update data analysis 1

--- static/js/index.js
+++ static/js/index.js
@@ -5,8 +5,10 @@ var country = d3.select("#country");
 var confirmed = d3.select("#confirmed");
 var recovered = d3.select("#recovered");
 var fatalities = d3.select("#fatalities");
-var lineChart = d3.select(".lineChart");
-var barChart = d3.select(".barChart");
+var chart1 = d3.select(".chart1>img");
+var chart2 = d3.select(".chart2>img");
+var desc_global = d3.selectAll(".desc_global");
+var desc_country = d3.selectAll(".desc_country");
 
 d3.select(window).on("load", onLoad);
 
@@ -19,7 +21,11 @@ function onLoad() {
 function changeOutput() {
     // Reset data from all.json
     var results = data;
- 
+
+    // Reset global description
+    desc_global.attr("style", "display: block;");
+    desc_country.attr("style", "display: none;");
+
     // Filter data
     var selection = country.property("value");
     results = results.filter(result => result["Country_Region"] === selection);
@@ -35,8 +41,14 @@ function changeOutput() {
     confirmed.text(stat_c);
     recovered.text(stat_r);
     fatalities.text(stat_f);
-    lineChart.attr("src", `static/images/chart1_${country_name}.png`);
-    barChart.attr("src", `static/images/chart2_${country_name}.png`);    
+    chart1.attr("src", `static/images/chart1_${country_name}.png`);
+    chart2.attr("src", `static/images/chart2_${country_name}.png`);    
+
+    // Change global description to country description
+    if (selection!=='Global') {
+        desc_global.attr("style", "display: none;");
+        desc_country.attr("style", "display: block;");
+    }
 
     return;
 };
